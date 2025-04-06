bacb4df193b2cb83855e62bf346f56c5d83102ec
Soumyadip Chatterjee <113227471+Soumyadip77777@users.noreply.github.com>
2025-04-06 23:04:57 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,6 @@ function App() {
           <Route path="/send" element={<SendMoney />} />
           <Route path="/transfer-success" element={<TransferSuccess />} />
         </Routes>
-         <App />
       </BrowserRouter>
       <ToastContainer />
     </>
